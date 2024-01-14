98871e3f000e32bf314cee29e9ab9eedc5e5953f
S.Kushevskiy <kushievskii@mail.ru>
2024-01-14 14:36:30 +0700

    feat: Добавлена сортировка заказа перед отправкой

--- src/components/common/Cart/utils/prepareOrderEmailMessage.ts
+++ src/components/common/Cart/utils/prepareOrderEmailMessage.ts
@@ -1,43 +1,68 @@
-import { SUB_CATEGORIES_MAP } from "../../../../constants/data";
+import { CATEGORIES_MAP, SUB_CATEGORIES_MAP } from "../../../../constants/data";
 import { CartItem } from "../../../../types/types";
 import { OrderFormData } from "../types";
+
+type PreparedList = Pick<
+  CartItem,
+  "amount" | "name" | "price" | "vendorCode"
+> & { categoryOrSubCategoryName: string };
 
 export const getEmailMessage = (
   { email, name, phone }: OrderFormData,
   list: CartItem[]
 ): FormData => {
-  let total = 0;
-  const rows = list.reduce<string>(
-    (acc, { amount, name, price, vendorCode, subCategoryId, categoryId }) => {
-      const sum = price * amount;
-      total += sum;
+  let totalPrice = 0;
+  let flowersCount = 0;
+  const rows = list
+    .reduce<PreparedList[]>(
+      (acc, { amount, categoryId, name, price, subCategoryId, vendorCode }) => {
+        const categoryOrSubCategoryName = subCategoryId
+          ? SUB_CATEGORIES_MAP[categoryId][subCategoryId].value
+          : CATEGORIES_MAP[categoryId].value;
 
-      // TODO Нет проверок, что SUB_CATEGORIES_MAP[categoryId] содержит categoryId
-      const subCategoryName = subCategoryId
-        ? SUB_CATEGORIES_MAP[categoryId][subCategoryId].value
-        : "";
+        acc.push({
+          amount,
+          vendorCode,
+          price,
+          name,
+          categoryOrSubCategoryName,
+        });
 
-      return (
-        acc +
-        `
+        return acc;
+      },
+      []
+    )
+    .sort((a, b) =>
+      a.categoryOrSubCategoryName.localeCompare(b.categoryOrSubCategoryName)
+    )
+    .reduce<string>(
+      (acc, { amount, name, price, vendorCode, categoryOrSubCategoryName }) => {
+        const sum = price * amount;
+        totalPrice += sum;
+        flowersCount += amount;
+
+        return (
+          acc +
+          `
         ------------
-        Артикул: ${subCategoryName} (${vendorCode})
+        Артикул: ${categoryOrSubCategoryName} (${vendorCode})
         Наименование: ${name}
         Цена: ${price}
         Количество: ${amount}
         Сумма: ${sum}₽\n`
-      );
-    },
-    ""
-  );
+        );
+      },
+      ""
+    );
 
   const formData = new FormData();
 
   formData.append("Покупатель", name);
   formData.append("Телефон", phone);
   formData.append("Почта", email);
-  formData.append("Сумма заказа", `${total}₽`);
+  formData.append("Сумма заказа", `${totalPrice}₽`);
   formData.append("Количество позиций", String(list.length));
+  formData.append("Количество товара", `${flowersCount}шт.`);
   formData.append("Заказ", rows);
 
   return formData;
