2224bc3f58ed26ba5d6c4d251a1dcb50ddee6f93
S.Kushevskiy <kushievskii@mail.ru>
2024-01-18 13:58:44 +0700

    fix: Обновление каталога

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -526,7 +526,7 @@ const CAT_1_SUBCAT_1: CatalogItem[] = [
     image: "1",
     categoryId: 1,
     subCategoryId: 1,
-    isAvailable: true,
+    isAvailable: false,
   },
   {
     id: 2,
@@ -671,7 +671,7 @@ const CAT_1_SUBCAT_1: CatalogItem[] = [
     image: "13",
     categoryId: 1,
     subCategoryId: 1,
-    isAvailable: true,
+    isAvailable: false,
   },
   {
     id: 14,
